8853f0eebc8cdc62a1e84454a5f83c5317412bcb
osmanforhad <osmanforhad93@gmail.com>
2022-03-08 15:15:54 +0600

    working with intregation of node js backend api with react js client area

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useRef } from "react";
 import { Form, Row, Col, Button } from "react-bootstrap";
 import Dropzone from "react-dropzone";
 import axios from "axios";
+import { API_URL } from "./utils/constants";
 
 const App = (props) => {
   const [file, setFile] = useState(null); // state for storing actual image
@@ -43,6 +44,31 @@ const App = (props) => {
 
   const handleOnSubmit = async (event) => {
     event.preventDefault();
+
+    try {
+      const { title, description } = state;
+      if (title.trim() !== "" && description.trim() !== "") {
+        if (file) {
+          const formData = new FormData();
+          formData.append("file", file);
+          formData.append("title", title);
+          formData.append("description", description);
+
+          setErrorMsg("");
+          await axios.post(`${API_URL}/upload`, formData, {
+            headers: {
+              "Content-Type": "multipart/form-data",
+            },
+          });
+        } else {
+          setErrorMsg("Please select a file to add.");
+        }
+      } else {
+        setErrorMsg("Please enter all the field values.");
+      }
+    } catch (error) {
+      error.response && setErrorMsg(error.response.data);
+    }
   };
 
   return (
